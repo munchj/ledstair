32bae0bfd52cc4fa4d58881facc9fbcdefa7df81
munchj <munch.juu@gmail.com>
2015-10-01 17:44:50 +0200

    Update ledstairs.ino

--- ledstairs/ledstairs.ino
+++ ledstairs/ledstairs.ino
@@ -3,11 +3,13 @@
 #include <Adafruit_PWMServoDriver\Adafruit_PWMServoDriver.h>
 #include <EspWifi.h>
 #include <Stair.h>
+#include <MotionSensor.h>
 
 
 Stair stair;
 EspWifi wifi;
-
+MotionSensor sensor1(A0);
+MotionSensor sensor2(A0);
 
 void setup() {
 	DBG.begin(9600);
@@ -18,9 +20,9 @@ void setup() {
 	stair.begin();
 	wifi.setStair(&stair);
 	wifi.connect(SSID, PASS, PORT);
-
-	pinMode(A0, INPUT);
-	pinMode(A1, INPUT);
+	
+	sensor1.begin(&stair);
+	sensor2.begin(&stair);
 
 	stair.flash();
 }
@@ -28,8 +30,11 @@ void setup() {
 
 
 void loop() {
+	sensor1.tick();
+	sensor2.tick();
 	wifi.tick();
 	stair.tick();
+	
 
 	//for (uint8_t pwmnum = 0; pwmnum < 16; pwmnum += 2) {
 	//	for (uint16_t i = 0; i<4096; i += 8) {
