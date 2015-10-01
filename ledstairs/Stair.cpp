2a43e9b95975a0235eefcb7054ca84004feaf5b6
munchj <munch.juu@gmail.com>
2015-10-01 12:43:15 +0200

    Light sequence 1

--- ledstairs/Stair.cpp
+++ ledstairs/Stair.cpp
@@ -1,5 +1,7 @@
 #include "Stair.h"
-#define DBG Serial
+#include <Constants.h>
+
+#define MAX_INTENSITY 4096
 
 
 uint8_t pwmMap[16] = { 0, 2, 4, 6, 8, 10, 12 ,14, 15, 13, 11, 9, 7, 5, 3, 1 };
@@ -65,15 +67,57 @@ void Stair::begin() {
 }
 
 void Stair::tick() {
-	//unsigned long now = millis();
+	unsigned long now = millis();
 
-	//int16_t t = (now - _functionInitTime) % 10000 ;
-	//float angle = ((float)t / 10000) * 6.28;
-	//for (uint8_t i = 0; i < LED_COUNT; i++)
-	//{
-	//	float phase = (2.0 * 3.1415 / 16.0) * (float)i;
-	//	_leds[i]->setIntensity((int16_t)((1.0 + sin(angle-phase)) * 2048));
-	//}
+	switch(_mode) {
+		case SINUSOIDE:
+		{
+			int16_t t = (now - _functionInitTime) % 10000 ;
+			float angle = ((float)t / 10000) * 6.28;
+			for (uint8_t i = 0; i < LED_COUNT; i++)
+			{
+				float phase = (2.0 * 3.1415 / 16.0) * (float)i;
+				_leds[i]->setIntensity((int16_t)((1.0 + sin(angle-phase)) * 2048));
+			}
+			break;
+		}
+		case LIGHT_SEQUENCE_1:
+		{
+			int16_t lightUpDuration = 1500;
+			int16_t lightDownDuration = 5000;
+			int16_t totalUpDuration = 15000;
+			int16_t phaseDuration = 1000;
+			long t = (now - _functionInitTime);
+			for (uint8_t i = 0; i < LED_COUNT; i++)
+			{
+				long tChannel = t - phaseDuration * i; //t + phase
+				
+				if(tChannel < 0 ||  tChannel > (lightUpDuration+lightDownDuration+totalUpDuration))
+				{
+					_leds[i]->setIntensity(0);	
+				}
+				else if(tChannel > 0 && tChannel <= lightUpDuration)
+				{
+					_leds[i]->setIntensity(tChannel  * MAX_INTENSITY / lightUpDuration);
+				}
+				else if(tChannel > lightUpDuration && tChannel <= (lightUpDuration+totalUpDuration))
+				{
+					_leds[i]->setIntensity(MAX_INTENSITY);
+				}
+				else if(tChannel > (lightUpDuration+totalUpDuration) && tChannel <= (lightUpDuration+totalUpDuration+lightDownDuration))
+				{
+					_leds[i]->setIntensity((lightUpDuration+totalUpDuration+lightDownDuration-tChannel)  * MAX_INTENSITY / lightDownDuration);
+				}
+				
+			}
+			break;
+		}		
+		default:
+		{
+			
+		}
+	}
+
 
 	for (uint8_t i = 0; i < LED_COUNT; i++)
 	{
